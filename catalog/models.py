ba47805b9f3984922222c48cd8aef8bd4872e6f4
dmitryvorot <vorotnikov7009@mail.ru>
2023-09-17 20:25:25 +0800

    initial admin

--- catalog/models.py
+++ catalog/models.py
@@ -7,17 +7,18 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(default='default_description', verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category =models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.PositiveIntegerField(verbose_name='Цена')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     last_modification_date = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     def __str__(self):
-        return f'{self.name}, {self.category}'
+        return f'{self.name}, {self.price}, {self.category}'
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        ordering = ('id',)
 
 
 class Category(models.Model):
@@ -30,3 +31,4 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ('id',)
