2d0573e60fb5c0103ad3a43125cefd0afa858b43
dmitryvorot <vorotnikov7009@mail.ru>
2023-09-17 19:48:37 +0800

    create models Product, Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(default='default_description', verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category =models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    last_modification_date = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}, {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(default='default_description', verbose_name='Описание')
+
+    def __str__(self):
+        return f'Категория: {self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
