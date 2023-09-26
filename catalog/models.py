b1408b523093c31c5ac72871a148a262add165ad
dmitryvorot <vorotnikov7009@mail.ru>
2023-09-27 01:59:08 +0800

    Add model Article

--- catalog/models.py
+++ catalog/models.py
@@ -32,3 +32,22 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ('id',)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='Slug')
+    content = models.TextField(max_length=10000, verbose_name='Содержание')
+    preview = models.ImageField(upload_to='article/', verbose_name='Превью', **NULLABLE)
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
+    number_of_views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ('id',)
+
